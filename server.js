0903f0cd7d35e8fc99d3061b96fbe8308b109218
joek-yo <jkihiu129@gmail.com>
2024-07-21 18:30:35 +0300

    Implement real-time chat using Socket.io

--- server.js
+++ server.js
@@ -1,33 +1,19 @@
 const express = require('express');
 const http = require('http');
-const socketio = require('socket.io');
+const { Server } = require('socket.io');
+const { ExpressPeerServer } = require('peer');
 const { v4: uuidV4 } = require('uuid');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = new Server(server);
 
-const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
-    debug: true,
+    debug: true
 });
 
 app.use('/peerjs', peerServer);
-
-
-// Connect to MongoDB
-connectDB();
-
-// Init Middleware
-app.use(express.json());
 app.use(express.static('public'));
-
-// Define Routes
-app.use('/api/auth', require('./routes/auth'));
 
 app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`);
@@ -40,14 +26,18 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-connected', userId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+
+        socket.on('message', message => {
+            io.to(roomId).emit('createMessage', `User ${userId}: ${message}`);
+        });
 
         socket.on('disconnect', () => {
-            socket.broadcast.to(roomId).emit('user-disconnected', userId);
+            socket.to(roomId).broadcast.emit('user-disconnected', userId);
         });
     });
 });
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+server.listen(5000, () => {
+    console.log('Server is running on port 5000');
+});
